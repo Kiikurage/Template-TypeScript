6ffafb48bc0149cb123087673aae48c560141c99
Kiikurage <y.kikura@gmail.com>
2021-04-08 04:34:27 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,51 @@
 module.exports = {
     root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-        createDefaultProgram: true,
-    },
-    plugins: ['prettier', '@typescript-eslint', 'react'],
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
+    plugins: ['import'],
+    extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings'],
     env: {
         es6: true,
-        node: true,
+        node: false,
     },
     ignorePatterns: ['./node_modules', './package.json', './package-lock.json'],
     rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/no-namespace': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'error',
         'no-inner-declarations': 'off',
         'semi': 'error',
         'space-infix-ops': 'error',
-        'prettier/prettier': [
-            'error',
-            {
-                tabWidth: 4,
-                singleQuote: true,
-                printWidth: 140,
-                quoteProps: 'consistent',
-            },
-        ],
-        'react/jsx-uses-react': 'error',
-        'react/jsx-uses-vars': 'error',
-        'react/react-in-jsx-scope': 'error',
     },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            plugins: ['@typescript-eslint'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: ['./tsconfig.json'],
+            },
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                'plugin:import/typescript',
+            ],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/no-use-before-define': 'off',
+                '@typescript-eslint/no-enum': 'off',
+                '@typescript-eslint/no-namespace': 'off',
+                '@typescript-eslint/no-non-null-assertion': 'error',
+                'no-restricted-syntax': [
+                    'error',
+                    {
+                        selector: 'TSEnumDeclaration',
+                        message: "Don't use enums. Use union type instead.",
+                    },
+                ],
+            },
+        },
+        {
+            files: ['.eslintrc.js', '.prettierrc.js', 'babel.config.js', 'jest.config.js'],
+            env: {
+                node: true,
+            },
+        },
+    ],
 };
